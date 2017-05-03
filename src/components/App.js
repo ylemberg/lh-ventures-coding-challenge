e47a0cc8998b8cd38b6f9923b4872dadb09d1fb5
Cezary Wojtkowski <cezary@gmail.com>
2017-05-03 14:16:31 -0700

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import InputForm from './InputForm';
+import BalanceOutput from './BalanceOutput';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h2>Little Accountant</h2>
+        <InputForm />
+        <br />
+        <BalanceOutput />
+      </div>
+    );
+  }
+}
+
+export default App;
